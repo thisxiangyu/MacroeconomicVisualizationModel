35e7c538c0a58d3910608863fcbf4f8553417fe8
swagy1 <983233194@qq.com>
2024-06-26 15:24:21 +0800

    PersonComponentUpdate

--- Assets/Scripts/People/PersonComponent.cs
+++ Assets/Scripts/People/PersonComponent.cs
@@ -2,10 +2,21 @@ using UnityEngine;
 
 public class PersonComponent : MonoBehaviour
 {
+    float 健康 = 100;
+
+    //人并没有善恶的属性，善恶是高概念，也没有利他和利己的区别，利他利己也是高概念。
+    //正确的细胞级的描述是区分人格的排他性、融他性、对外消化力
+
+    float 人格排他性 = 100; //抵御外物对自身影响的趋势
+
+    float 人格融他性 = 100; //允许外物对自身影响的趋势
+
+    float 对外消化力 = 100; //对外物施加影响的趋势
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+         
     }
 
     // Update is called once per frame
